b2c88f8848f50f2d372707603026ab52e1e971d1
Joshua <joshuakassim6@gmail.com>
2024-02-27 15:32:40 +0200

    Configured sessions

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -10,7 +11,11 @@ db = SQLAlchemy(model_class=Base)
 
 # Create the app
 app = Flask(__name__)
+# Configure session to use filesystem
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 # Configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sparkED.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sparkED.db'
 # Initialize the app with the extension
 db.init_app(app)
